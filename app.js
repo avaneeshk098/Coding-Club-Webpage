48c4cf156852dd68ac15b19418d6a9f953c93298
Avaneesh Kumar <avaneeshk098@gmail.com>
2020-05-27 20:09:50 +0530

    added about us section (navigation implentation yet pending)

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const burger = document.getElementsByClassName("burger")[0]; //Gets the burger button object
 const nav = document.getElementsByClassName("nav-links")[0]; //Gets the unoordered list of links in the nav-bar
 const logo = document.getElementsByClassName("navbar-main")[0];// Gets the container div of logo and burger button
+const navbar = document.getElementsByClassName("navbar")[0];
 
+window.onscroll = function() {myFunction()};
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 burger.addEventListener("click", () => {
 	nav.classList.toggle("active"); //triggers active class of nav-links
 	logo.classList.toggle("active"); // triggers active class of logo
